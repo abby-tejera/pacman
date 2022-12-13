9b68bf1b598e380c4131cac84aed9efff43f5242
abby-tejera <atejera@oberlin.edu>
2022-12-13 01:32:37 -0500

    Disable drawn walls and use wall distribution.

--- src/components/Walls.tsx
+++ src/components/Walls.tsx
@@ -1,6 +1,20 @@
+import { wallDistribution, gridSize } from '../constants/maze'
 import styles from '../styles/Walls.module.css'
 
 export function Walls() {
+    return (
+        <g>
+            {wallDistribution.map((row, j) => row.map((n, i) => (
+                <>
+                {n == 1 ? (
+                    <rect key={`${i},${j}`} x={i*gridSize} y={j*gridSize} width={gridSize} height={gridSize} className={styles.wall} />
+                ) : null}
+                </>
+            )))}
+        </g>
+    )
+
+    // Disabled drawn walls due to inconsistency with the wall distribution.
     return (
         <g className={styles.walls}>
             <g id="Layer_1">
